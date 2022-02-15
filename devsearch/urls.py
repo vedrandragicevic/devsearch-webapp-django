e097c3c7df38e3d490a2a22133ec10821ed99c2d
Vedran Dragičević <vdragicevic@iolap.com>
2022-02-15 14:59:52 +0100

    added reset password template and styled it

--- devsearch/urls.py
+++ devsearch/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 # TWO IMPORT BELOW HELP CREATE URLs FOR STATIC FILES
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 
 """
     SEPERATE URLS INTO MULTIPLE MODULES BASED ON THE APP
@@ -26,6 +27,12 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('projects/', include('projects.urls')),
     path('', include('users.urls')),
+
+    # Class based views (use .as_view())
+    path('reset_password/', auth_views.PasswordResetView.as_view(template_name="reset_password.html"), name="reset_password"),
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(), name="password_reset_done"),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete")
 ]
 
 # CREATES URls FOR STATIC FILES
