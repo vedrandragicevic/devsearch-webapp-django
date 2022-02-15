6a6f2f48a4c0c83291ce7215e9c43066a85290bd
Vedran Dragičević <vdragicevic@iolap.com>
2022-02-15 16:39:24 +0100

    added templates for password reset steps

--- devsearch/urls.py
+++ devsearch/urls.py
@@ -30,9 +30,9 @@ urlpatterns = [
 
     # Class based views (use .as_view())
     path('reset_password/', auth_views.PasswordResetView.as_view(template_name="reset_password.html"), name="reset_password"),
-    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(), name="password_reset_done"),
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
-    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete")
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name="reset_password_sent.html"), name="password_reset_done"),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="reset.html"), name="password_reset_confirm"),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name="reset_password_complete.html"), name="password_reset_complete")
 ]
 
 # CREATES URls FOR STATIC FILES
